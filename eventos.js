ff559f2d80f818ffd289206f06801d16bad7d06b
Pilar Figueroa <mpilarfigueroa.96@gmail.com>
2021-11-08 20:41:17 -0300

    agregando nuevas funcionalidades

--- eventos.js
+++ eventos.js
@@ -1,32 +1,32 @@
 const btn = document.querySelector('button');
 
-// const entrada = (element) => {
-//     element.style.backgroundColor = 'red'
-// }
+const entrada = (element) => {
+    element.style.backgroundColor = 'red'
+}
 
-// const salida = (element) => {
-//     element.style.backgroundColor = 'green'
-// }
+const salida = (element) => {
+    element.style.backgroundColor = 'green'
+}
 
-// btn.addEventListener('mouseover', () => {
-//     entrada(btn)
-// })
+btn.addEventListener('mouseover', () => {
+    entrada(btn)
+})
 
-// btn.addEventListener('mouseout', () => {
-//     salida(btn)
-// })
+btn.addEventListener('mouseout', () => {
+    salida(btn)
+})
 
-// const crearCaja = (message) => {
-//     const container = document.querySelector('.contenedor')
-//     const div = document.createElement('div');
-//     div.style.width = '200px';
-//     div.style.height = '200px';
-//     div.style.backgroundColor = 'red';
-//     div.style.margin = '20px';
-//     div.innerText = message;
-//     container.appendChild(div);
-// }
+const crearCaja = (message) => {
+    const container = document.querySelector('.contenedor')
+    const div = document.createElement('div');
+    div.style.width = '200px';
+    div.style.height = '200px';
+    div.style.backgroundColor = 'red';
+    div.style.margin = '20px';
+    div.innerText = message;
+    container.appendChild(div);
+}
 
-// btn.addEventListener('click', () => {
-//     crearCaja('Hola soy una caja');
-// });
+btn.addEventListener('click', () => {
+    crearCaja('Hola soy una caja');
+});
--- script.js
+++ script.js
@@ -1,68 +1,64 @@
-// const formulario = document.querySelector("#formulario");
-// let inputFullname = document.querySelector("#fullname");
-// let inputEmail = document.querySelector("#email");
-// let inputPassword = document.querySelector("#password");
-// let inputRepeatPassword = document.querySelector("#repeatPassword");
-// let btnSubmit = document.querySelector("#submit");
+const formulario = document.querySelector("#formulario");
+let inputFullname = document.querySelector("#fullname");
+let inputEmail = document.querySelector("#email");
+let inputPassword = document.querySelector("#password");
+let inputRepeatPassword = document.querySelector("#repeatPassword");
+let inputCheck = document.querySelector("#checkId");
+let select = document.querySelector("#cars");
+let btnSubmit = document.querySelector("#submit");
 
-// let fullname, email, pass;
+let fullname, email, pass;
 
-// const getData = () => {
-//     fullname = inputFullname.value;
-//     email = inputEmail.value;
-//     pass = inputPassword.value;
+const getData = () => {
+    fullname = inputFullname.value;
+    email = inputEmail.value;
+    pass = inputPassword.value;
     
-//     inputFullname.value = "";
-//     inputEmail.value = "";
-//     inputPassword.value = "";
-// }
+    formulario.reset() 
+}
 
-// const printData = () => {
-//     let box = document.querySelector(".data-box");
-//     let p_password = document.querySelector(".password");
-//     let p_fullname = document.querySelector(".fullname");
-//     let p_email = document.querySelector(".email");
+const printData = () => {
+    let box = document.querySelector(".data-box");
+    let p_password = document.querySelector(".password");
+    let p_fullname = document.querySelector(".fullname");
+    let p_email = document.querySelector(".email");
 
-//     p_fullname.innerText = `Nombre: ${fullname}`;
-//     p_email.innerText = `Email: ${email}`;
-//     p_password.innerText = `Pasword: ${pass}`;
-//     box.style.display = 'block';
-// }
+    p_fullname.innerText = `Nombre: ${fullname}`;
+    p_email.innerText = `Email: ${email}`;
+    p_password.innerText = `Pasword: ${pass}`;
+    box.style.display = 'block';
+}
 
-// const handleOnChange = (event) => {
-//     let p_fullname = document.querySelector(".fullname"); 
-//     console.log(event.target.value);
-//     p_fullname.innerText += ` ${event.target.value}`;
-// }
+const validatePassword = () => {
+    const success = document.querySelector('#password-success');
+    const alert = document.querySelector('#password-alert');
+    if (inputPassword.value === inputRepeatPassword.value) {
+        success.classList.remove('d-none');
+        alert.classList.add('d-none');
+        btnSubmit.removeAttribute('disabled', 'disabled');
+        return true
+    } else {
+        alert.classList.remove('d-none');
+        success.classList.add('d-none');
+        btnSubmit.setAttribute('disabled', 'disabled');
+        return false
+    }
+}
 
-// const validatePassword = () => {
-//     const success = document.querySelector('#password-success');
-//     const alert = document.querySelector('#password-alert');
-//     if (inputPassword.value === inputRepeatPassword.value) {
-//         success.classList.remove('d-none');
-//         btnSubmit.removeAttribute('disabled', 'disabled');
-//     } else {
-//         alert.classList.remove('d-none');
-//         btnSubmit.setAttribute('disabled', 'disabled');
-//     }
-// }
+// Acceder a un input type check
+/*
 
-// btnSubmit.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     getData();
-//     printData();
-// });
+inputCheck.checked -> true/false
 
-// btnSubmit.onclick = (e) => {
-//     e.preventDefault();
-//     getData();
-//     printData();   
-// }
+*/
 
-// inputFullname.addEventListener('click', (e) => console.log(e.target.value));
+btnSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (validatePassword()) {
+        getData();
+    } else {
+        alert('Tus datos son incorrectos')
+    }
+});
 
-// inputFullname.onchange = (e) => {
-//     handleOnChange(e);
-// }
-
-// inputRepeatPassword.onchange = validatePassword;
+inputRepeatPassword.addEventListener('change', validatePassword)
